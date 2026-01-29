c716f6e7dc8a5384a5be4b2135928adfffbde90c
AnthonyRG <anthonyrizk89@gmail.com>
2026-01-29 16:16:40 +0100

    added search field for films, added a new favicon

--- src/app/(dashboard)/films/page.tsx
+++ src/app/(dashboard)/films/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import api from "@/lib/api"
 import {FilmResponseDto} from "@/types/types"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
@@ -9,37 +9,68 @@ import Link from "next/link";
 
 export default function UsersPage() {
     const [films, setFilms] = useState<FilmResponseDto[]>([])
+    const [sfilms, setSearchFilms] = useState<FilmResponseDto[]>([])
+    const [searchterm, setSearchterm] = useState();
+
+    const inputRef = useRef(null);
+    const bodyRef  = useRef(null);
 
     useEffect(() => {
-        api.get("/film/all/")
-            .then(res => setFilms(res.data))
-            .catch(err => {
-                const status = err.response?.status;
-                const message = err.response?.message;
-                console.error("Access Denied with message: ", message, " and status: ", status);
-            })
-    }, [])
+        const searchterm = inputRef?.current.value ?? "";
+        console.log("search for film:  " + searchterm);
+        if(searchterm == ""){
+            api.get("/test/films")
+                .then(res => setFilms(res.data))
+                .catch(err => {
+                    const status = err.response?.status;
+                    const message = err.response?.message;
+                    console.error("Access Denied with message: ", message, " and status: ", status);
+                })
+            }
+        else {
+            api.get("/api/film/search/"+searchterm)
+                .then(res => setFilms(res.data))
+                .catch(err => {
+                    const status = err.response?.status;
+                    const message = err.response?.message;
+                    console.error("Access Denied with message: ", message, " and status: ", status);
+                })
+            }
+    }, [searchterm])
+
+
+    function handleClick(){
+    console.log("inputRef: " +  inputRef.current.value);
+        setSearchterm(inputRef.current.value);
+    }
+
+
+
 
     return (
         <div className="p-8">
             <h1 className="text-2xl font-bold mb-4">Film Directory</h1>
+            <input type="text" ref={inputRef} placeholder="Search film"/>
+            <button onClick={ handleClick }>
+                Search
+            </button>
             <Table>
                 <TableHeader>
                     <TableRow>
                         <TableHead>Title</TableHead>
-                        <TableHead>genre</TableHead>
-                        <TableHead>casta</TableHead>
-                        <TableHead>ageRestriction</TableHead>
-                        <TableHead>awards</TableHead>
-                        <TableHead>languages</TableHead>
-                        <TableHead>aspectRatio</TableHead>
-                        <TableHead>colorStatus</TableHead>
-                        <TableHead>cameraUsed</TableHead>
+                        <TableHead>Genre</TableHead>
+                        <TableHead>Cast</TableHead>
+                        <TableHead>Age restriction</TableHead>
+                        <TableHead>Awards</TableHead>
+                        <TableHead>Languages</TableHead>
+                        <TableHead>Aspect ratio</TableHead>
+                        <TableHead>Color</TableHead>
+                        <TableHead>Camera</TableHead>
                         <TableHead>Reviews</TableHead>
                         <TableHead>Details</TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
+                <TableBody ref={bodyRef}>
                     {films.map((film) => (
                         <TableRow key={film.id}>
                             <TableCell>{film.title}</TableCell>
